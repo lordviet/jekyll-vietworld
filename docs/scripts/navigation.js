e5c819605b5f51555a7bc41881641b28c85a73a6
lordviet <viet1231@abv.bg>
2019-08-07 17:32:35 +0300

    Added sctipts for navbar

---
+++ docs/scripts/navigation.js
@@ -0,0 +1,8 @@
+function openNav() {
+    // document.getElementById("navbar").style.width = "100%";
+    document.getElementById("navbar").style.display = "block";
+}
+
+function closeNav(){
+    document.getElementById("navbar").style.display = "none";
+}
