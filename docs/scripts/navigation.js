1eaff653c82d3d9feac88c99e43c524ddb4baec5
lordviet <viet1231@abv.bg>
2019-08-07 22:21:08 +0300

    Navbar update

--- docs/scripts/navigation.js
+++ docs/scripts/navigation.js
@@ -1,8 +1,14 @@
 function openNav() {
     // document.getElementById("navbar").style.width = "100%";
-    document.getElementById("navbar").style.display = "block";
+    // document.getElementById("navbar").style.display = "block";
+    var element = document.getElementById("navbar");
+    element.classList.remove("site-nav-hidden");
+    element.classList.add("site-nav-visible")
 }
 
-function closeNav(){
-    document.getElementById("navbar").style.display = "none";
+function closeNav() {
+    // document.getElementById("navbar").style.display = "none";
+    var element = document.getElementById("navbar");
+    element.classList.remove("site-nav-visible");
+    element.classList.add("site-nav-hidden")
 }
