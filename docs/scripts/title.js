72b820bfce71628428e3a3ef002e4dcd88a306a9
lordviet <viet1231@abv.bg>
2019-08-09 01:19:14 +0300

    Index of prototype

--- docs/scripts/title.js
+++ docs/scripts/title.js
@@ -1,6 +1,6 @@
 function changingTitle() {
     let title = document.getElementById("title");
-    let arr = ["Dev", "Magician", "ML", "JS", "Python", "Comics", "Nerd", "Books", "Workouts", "Sup?"];
+    let arr = ["Dev", "Magician", "ML", "JS", "Python","Start-ups", "Dreamer", "Comics", "Nerd", "Books", "Workouts", "Sup?"];
     let i = 0, howManyTimes = arr.length;
 
     function loopThrough() {
